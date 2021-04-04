ff89fe2ff59f0c636cc4742aa3f8dbf8ab6ff1ee
Mohamed Fadl <53804570+mohamedfadl285@users.noreply.github.com>
2021-04-05 01:09:01 +0200

    Update Config file

--- inc/config.php
+++ inc/config.php
@@ -2,5 +2,5 @@
 
 const DB_HOST = 'localhost';
 const DB_NAME = 'auth_php';
-const DB_USERNAME = 'fadl';
-const DB_PASSWORD = 123456;
+const DB_USERNAME = 'Your DB username';
+const DB_PASSWORD = 'Your DB password';
