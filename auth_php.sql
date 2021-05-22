f8ba1e503e13fc9b42d196c151a1acec52f2eff0
Mohamed Fadl <mohamed.fadl2852@gmail.com>
2021-05-22 13:23:53 +0200

    update db file

--- auth_php.sql
+++ auth_php.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.2
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 23, 2021 at 01:31 PM
--- Server version: 10.4.10-MariaDB
--- PHP Version: 7.3.12
+-- Generation Time: May 22, 2021 at 01:20 PM
+-- Server version: 10.4.18-MariaDB
+-- PHP Version: 8.0.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -21,6 +20,11 @@ SET time_zone = "+00:00";
 --
 -- Database: `auth_php`
 --
+
+DROP DATABASE IF EXISTS `auth_php`;
+
+CREATE DATABASE IF NOT EXISTS `auth_php` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `auth_php`;
 
 -- --------------------------------------------------------
 
@@ -41,8 +45,9 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `mobile`, `password`) VALUES
-(2, 'Ahmed', 'ahmed@mail.com', '01012345678', '$2y$10$Nq.iKwQpJZ.oGjZ9TC0U5.VhaDi5s.WA4qBh7onYFKK/Y9iW1UEMi'),
-(3, 'ahmed', 'mido@mail.com', '01012345678', '$2y$10$fipwVrHMAty0fgJSn5NBSOWNhle7u5xwZJqBYTdMEdxTKFA5tevAa');
+(2, 'Ahmed', 'fadl@mail.com', '01012345678', '$2y$10$Nq.iKwQpJZ.oGjZ9TC0U5.VhaDi5s.WA4qBh7onYFKK/Y9iW1UEMi'),
+(3, 'ahmed', 'mido@mail.com', '01012345678', '$2y$10$fipwVrHMAty0fgJSn5NBSOWNhle7u5xwZJqBYTdMEdxTKFA5tevAa'),
+(7, 'hossam', 'hossam@mail.com', '01010782135', '$2y$10$y7b4anNBRTvr1IV4O/zAkuzdepjqONc6p/x50j88kwXhUVulexPGW');
 
 --
 -- Indexes for dumped tables
@@ -62,7 +67,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
